3c8636327eaa01488ed49a12d4f0553e9e2bf287
zacrogers <38870655+zacrogers@users.noreply.github.com>
2021-08-07 15:04:52 +1200

    Create main.h

---
+++ main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+// DC Motor pins
+#define MOT_D1   4
+#define MOT_D2   5
+#define MOT_IN1  6
+#define MOT_IN2  7
+#define MOT_SF   14
+#define MOT_EN   15
+#define MOT_SLEW 16
+#define MOT_INV  17
+
+// Servo pins
+#define SERVO1_PIN 47
+#define SERVO2_PIN 48
+
+// Solenoid pins
+#define SOL1  49
+#define SOL2  46
+
+#endif /*MAIN_H*/
